c508c7ce88730d9d22ee60d2d312c7a9a852f5c1
Davi Chechetto <davichechetto@gmail.com>
2025-06-23 14:52:32 -0300

    Co-authored-by: Igor da Silva Antunes <SantunesIgor@users.noreply.github.com>

---
+++ .angular/cache/19.2.15/Doa-facil/vite/deps/primeng_api.js
@@ -0,0 +1,37 @@
+import {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+} from "./chunk-3GDVFC4Y.js";
+import "./chunk-6EMZLCQL.js";
+import "./chunk-MTES5EJZ.js";
+import "./chunk-S35MAB2V.js";
+export {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+};
---
+++ .angular/cache/19.2.15/Doa-facil/vite/deps/primeng_config.js
@@ -0,0 +1,16 @@
+import {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+} from "./chunk-GCINQWIM.js";
+import "./chunk-3GDVFC4Y.js";
+import "./chunk-6EMZLCQL.js";
+import "./chunk-MTES5EJZ.js";
+import "./chunk-S35MAB2V.js";
+export {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+};
